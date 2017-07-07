d30a9afb579b760aeae67684262af499489bb688
SaschaSei <saschasei@googlemail.com>
2017-07-07 15:46:24 +0200

    Added correct line breaks to not have more than 80 chars per line.

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # Entertainment Center
-This is a simple program that shows some of my favourite films and links to their corresponding trailers.
+This is a simple program that shows some of my favourite films and links to
+their corresponding trailers.
 
 
 ## Requirements
 
-This was coded in **Python 2.7.13** so you have to have at this version of Python installed. You can dowload this verson of Python [here](https://www.python.org/downloads/).
+This was coded in **Python 2.7.13** so you have to have at this version of
+Python installed. You can dowload this verson of Python
+[here](https://www.python.org/downloads/).
 
 ## How To Run The Program
 Simply double click the **entertainment_center.py** file.
 
 ## Contributors
-This program uses fresh_tomatoes.py originally from Udacity. 
+This program uses fresh_tomatoes.py originally from Udacity.
--- entertainment_center.py
+++ entertainment_center.py
@@ -16,24 +16,28 @@ avatar = media.Movie(
 
 pulp_fiction = media.Movie(
     "Pulp Fiction", "",
-    "https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg",
+    "https://upload.wikimedia.org/wikipedia/en/3/3b/"\
+    "Pulp_Fiction_%281994%29_poster.jpg",
     "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
 
 school_of_rock = media.Movie(
     "School of Rock", "Jack Black teaching Rock",
-    "https://upload.wikimedia.org/wikipedia/en/thumb/1/11/School_of_Rock_Poster.jpg/220px-School_of_Rock_Poster.jpg",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/1/11/"\
+    "School_of_Rock_Poster.jpg/220px-School_of_Rock_Poster.jpg",
     "https://www.youtube.com/watch?v=XCwy6lW5Ixc")
 
 lucky_number_slevin = media.Movie(
     "Lucky Number S7evin",
     "Two underworld bosses, a mysterious hitman, and some guy in between",
-    "https://upload.wikimedia.org/wikipedia/en/a/a0/Lucky_Number_Slevin_Theater_Poster.JPG",
+    "https://upload.wikimedia.org/wikipedia/en/a/a0/"\
+    "Lucky_Number_Slevin_Theater_Poster.JPG",
     "https://www.youtube.com/watch?v=fVIUEcizkPc")
 
 full_metal_jacket = media.Movie(
     "Full Metal Jacket",
     "The story of young Americans who become soldiers in the Vietnam war.",
-    "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg",
+    "https://upload.wikimedia.org/wikipedia/en/9/99/"\
+    "Full_Metal_Jacket_poster.jpg",
     "https://www.youtube.com/watch?v=x9f6JaaX7Wg")
 
 movies = [toy_story, avatar, pulp_fiction, school_of_rock, lucky_number_slevin,
